4c625f61cc0482c870510c6743d1c95bf6eb73ad
IjjazAhmad <ijjazahmad705@gmail.com>
2023-10-28 12:32:39 +0500

    first commit

---
+++ app/src/main/java/com/example/img_button/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.img_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
